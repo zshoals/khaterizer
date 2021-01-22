04ba79045287f80c2a25f9d506f586db5f2da9fd
z <zshoals@gmail.com>
2021-01-22 12:50:33 -0600

    better names

--- Sources/khaterizer/math/KtzDegrees.hx
+++ Sources/khaterizer/math/KtzDegrees.hx
@@ -26,11 +26,11 @@ abstract KtzDegrees(Float) {
 		return new KtzDegrees((this / f) % 360);
 	}
 
-	public inline function toUnitKtzVec2(): KtzVec2 {
+	public inline function makeUnitKtzVec2(): KtzVec2 {
 		return new KtzVec2(Math.cos(KtzMath.deg2rad(this)), Math.sin(KtzMath.deg2rad(this)));
 	}
 
-	public inline function fromKtzVec2(vec:KtzVec2): KtzDegrees {
+	public inline function angleFromKtzVec2(vec:KtzVec2): KtzDegrees {
 		return new KtzDegrees(KtzMath.rad2deg(Math.atan2(vec.x, vec.y)));
 	}
 }
