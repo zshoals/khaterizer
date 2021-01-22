6f11941f99bfd90e1aa174e079d72304965d7b33
z <zshoals@gmail.com>
2021-01-22 15:11:57 -0600

    formatting

--- Sources/khaterizer/math/KtzDegrees.hx
+++ Sources/khaterizer/math/KtzDegrees.hx
@@ -10,19 +10,19 @@ abstract KtzDegrees(Float) {
 		return this % 360;
 	}
 
-	public inline function add(f:KtzDegrees): KtzDegrees {
+	public inline function add(f: KtzDegrees): KtzDegrees {
 		return new KtzDegrees((this + f.toFloat()) % 360);
 	}
 
-	public inline function sub(f:KtzDegrees): KtzDegrees {
+	public inline function sub(f: KtzDegrees): KtzDegrees {
 		return new KtzDegrees((this - f.toFloat()) % 360);
 	}
 
-	public inline function mult(f:KtzDegrees): KtzDegrees {
+	public inline function mult(f: KtzDegrees): KtzDegrees {
 		return new KtzDegrees((this * f.toFloat()) % 360);
 	}
 
-	public inline function div(f:KtzDegrees): KtzDegrees {
+	public inline function div(f: KtzDegrees): KtzDegrees {
 		return new KtzDegrees((this / f.toFloat()) % 360);
 	}
 
@@ -30,7 +30,7 @@ abstract KtzDegrees(Float) {
 		return new KtzVec2(Math.cos(KtzMath.deg2rad(this)), Math.sin(KtzMath.deg2rad(this)));
 	}
 
-	public static inline function angleFromKtzVec2(vec:KtzVec2): KtzDegrees {
+	public static inline function angleFromKtzVec2(vec: KtzVec2): KtzDegrees {
 		return new KtzDegrees(KtzMath.rad2deg(Math.atan2(vec.y, vec.x)));
 	}
 }
--- Sources/khaterizer/math/KtzMath.hx
+++ Sources/khaterizer/math/KtzMath.hx
@@ -3,25 +3,25 @@ package khaterizer.math;
 class KtzMath {
 	public inline static var EPSILON:Float = 0.0000001;
 
-	public inline static function truncate(number:Float, precision:Int): Float {
+	public inline static function truncate(number: Float, precision: Int): Float {
 		var num = number;
 		num = num * Math.pow(10, precision);
 		return Math.round(num) / Math.pow(10, precision);
 	}
 
-	public inline static function clamp(val:Float, minimum:Float, max:Float): Float {
+	public inline static function clamp(val: Float, minimum: Float, max: Float): Float {
 		return Math.min(Math.max(val, minimum), max);
 	}
 
-	public inline static function deg2rad(degrees:Float): Float {
+	public inline static function deg2rad(degrees: Float): Float {
 		return degrees * Math.PI / 180;
 	}
 
-	public inline static function rad2deg(radians:Float): Float {
+	public inline static function rad2deg(radians: Float): Float {
 		return radians * 180 / Math.PI;
 	}
 
-	public inline static function areClose(float1:Float, float2:Float, ?customEpsilon:Float): Bool {
+	public inline static function areClose(float1: Float, float2: Float, ?customEpsilon: Float): Bool {
 		if (customEpsilon == null) {
 			customEpsilon = EPSILON;
 		}
@@ -29,7 +29,7 @@ class KtzMath {
 		return Math.abs(float1 - float2) < customEpsilon;
 	}
 
-	public inline static function withinRotationTolerance(degrees:Float, ?customEpsilon:Float): Bool {
+	public inline static function withinRotationTolerance(degrees: Float, ?customEpsilon: Float): Bool {
 		degrees = Math.abs(degrees);
 
 		if (customEpsilon == null) {
