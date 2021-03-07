312a793d300ada6140b1c79f49b55aa9d92ebcab
z <zshoals@gmail.com>
2021-03-07 09:05:51 -0600

    camera component api

---
+++ Sources/khaterizer/components/graphics/KtzCameraComp.hx
@@ -0,0 +1,28 @@
+package khaterizer.components.graphics;
+
+import khaterizer.math.KtzVec2;
+import khaterizer.math.KtzVec2T;
+import khaterizer.utils.KtzRegion2D;
+import khaterizer.math.KtzFastMat3;
+import khaterizer.math.KtzDegrees;
+import khaterizer.math.KtzNormalizedRange;
+import kha.Image;
+
+final class KtzCameraComp /*implements IComponent*/ {
+	public var position: KtzVec2;
+	public var rotation: KtzDegrees;
+	public var zoomFactor: Float;
+	
+	/**
+		The region of the primary Window between 0 and 1 on the X and Y axes that this camera draws its surface to.
+	**/
+	public var screenRegion: KtzRegion2D<KtzNormalizedRange>;
+	
+	private var projectionMatrix: KtzFastMat3;
+	private var viewMatrix: KtzFastMat3;
+
+	private var drawSurface: Image;
+
+	public function new() {
+	}
+}
--- Sources/khaterizer/import.hx
+++ Sources/khaterizer/import.hx
@@ -1,7 +1,2 @@
-import ecx.Entity;
-import ecx.Wire;
-import ecx.Family;
-
 import khaterizer.math.KtzMath;
 import khaterizer.math.KtzVec2Extension;
-import khaterizer.math.KtzVec2;
