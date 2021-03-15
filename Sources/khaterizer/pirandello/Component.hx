2083734f450a809bfc452b15ffa47f5f33be905e
z <zshoals@gmail.com>
2021-03-15 11:41:51 -0500

    starting over with ECS. again

--- Sources/khaterizer/pirandello/Component.hx
+++ Sources/khaterizer/pirandello/Component.hx
@@ -1,29 +1,9 @@
 package khaterizer.pirandello;
 
-import khaterizer.pirandello.data.ComponentType;
-import haxe.ds.Option;
-
-
 /**
 	The base component class that all other components are derived from.
 
-	All components in Khaterizer are pooled.
+	All components in Khaterizer are pooled. As such, free() must be overridden to reset the component to a default state.
 **/
 class Component {
-	public var name: Option<String>;
-	public var componentType(default, null): ComponentType;
-	public var insidePool:Bool;
-
-	public function new(?name: String) {
-		if (name != null) {
-			this.name = Some(name);
-		}
-		else {
-			this.name = None;
-		}
-
-		//Weird, but works.
-		this.componentType = this;
-		this.insidePool = true;
-	}
 }
