7307c53c33947d836302799e60797bba919f1625
z <zshoals@gmail.com>
2021-03-15 20:10:06 -0500

    progress, component array generation works

--- Sources/khaterizer/pirandello/World.hx
+++ Sources/khaterizer/pirandello/World.hx
@@ -7,17 +7,66 @@ import haxe.ds.Vector;
 final class World {
 	private var activeEntities: Vector<Bool>; //should be some set of bitflags
 	public var activeEntityCount: Int;
+	public var maxEntities: Int;
 	public var manager: EntityManager;
 
 	private var components: Map<ComponentType, Vector<Component>>;
 	private var systems: Map<SystemType, System>;
 
-	// public function new(configuration: WorldConfiguration) {
-	// 	for 
-	// }
+	public function new(configuration: WorldConfiguration) {
+		this.maxEntities = configuration.entityMaximumCapacity;
+		this.activeEntityCount = 0;
+		this.activeEntities = new Vector<Bool>(this.maxEntities);
+		for (ent in this.activeEntities) {
+			ent = false;
+		}
+
+		this.components = new Map<ComponentType, Vector<Component>>();
+		for (componentType in configuration.componentsToRegister) {
+			//Careful. This is where we preallocate all of our components.
+			//This gets memory intensive fast, requiring either low entity counts or sparse components.
+			//We need to find a way to not just do it the stupid way.
+			var backingStorage = createStorage(componentType, maxEntities);
+			for (i in 0...backingStorage.length) {
+				backingStorage[i] = Type.createEmptyInstance(componentType);
+				backingStorage[i].initialize();
+			}
+			this.components.set(componentType, backingStorage);
+		}
+
+		// this.systems = new Map<SystemType, System>();
+		// for (systemType in configuration.systemsToRegister) {
+		// 	this.systems[systemType] = 
+		// }
+	}
+
+	public function retrieveStorage<T: Class<Component>>(type: T): Vector<T> {
+		if (components.exists(type)) {
+			return cast components.get(type);
+		}
+		else {
+			throw 'Tried to access a non-existent component backing storage "$type". Verify this component type was added to the world configuration.';
+			return null;
+		}
+	}
+
+	public function retrieveSystem<T: Class<System>>(type: T): T {
+		if (systems.exists(type)) {
+			return cast systems.get(type);
+		}
+		else {
+			throw 'Tried to access a non-existent system "$type". Verify this system type was added to the world configuration.';
+			return null;
+		}
+	}
 
 	@:generic
-	private inline function resolveComponent<T: ComponentType>(type: T, comps: Vector<Component>): Vector<T> {
+	private inline function createStorage<T>(type: Class<T>, len: Int): Vector<T> {
+		return new Vector<T>(len);
+	}
+
+	@:generic
+	private inline function resolveStorage<T: ComponentType>(type: Class<T>, comps: Vector<Component>): Vector<T> {
 		return (cast comps: Vector<T>);
 	}
 
@@ -29,14 +78,14 @@ final class World {
 }
 
 final class WorldConfiguration {
-	public var componentsToRegister: Array<Class<Component>>;
-	public var systemsToRegister: Array<Class<System>>;
+	public var componentsToRegister: Array<ComponentType>;
+	public var systemsToRegister: Array<SystemType>;
 	public var entityMaximumCapacity: Int;
 	public var sameComponentStorageSize: Int;
 
-	public function new(?entityMaximumCapacity: Int = 10000, ?sameComponentStorageSize = 1) {
-		this.componentsToRegister = new Array<Class<Component>>();
-		this.systemsToRegister = new Array<Class<System>>();
+	public function new(?entityMaximumCapacity: Int = 1000, ?sameComponentStorageSize = 1) {
+		this.componentsToRegister = new Array<ComponentType>();
+		this.systemsToRegister = new Array<SystemType>();
 		this.entityMaximumCapacity = entityMaximumCapacity;
 		this.sameComponentStorageSize = sameComponentStorageSize;
 		assert(sameComponentStorageSize > 0, "Must have storage for at least one component, otherwise what's the point?");
@@ -52,5 +101,9 @@ final class WorldConfiguration {
 		for (type in types) {
 			this.systemsToRegister.push(type);
 		}
+	}
+
+	public function registerPhasedSystems(types: Phase): Void {
+		var sys
 	}
 }
